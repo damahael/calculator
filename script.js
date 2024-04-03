9a621173f4dac8cc6c9de0a36b4f6a8f2caf706b
wangunderline <wangwangrian@gmail.com>
2024-04-03 18:54:35 -0300

    Make operations by interacting with the calculator
    The display is updated whenever there's an interaction with the buttons. All was done with click eventListeners and by storing values in variables for the first and second numbers. The separation is made by the operatorValue variable, which is called when there's a click in any operator. Ifs do the work.

--- script.js
+++ script.js
@@ -1,3 +1,30 @@
+const digits = document.querySelector("#digits");
+
+function addDigits() {
+  for (let i = 9; i > -1; i--) {
+    const digit = document.createElement("button");
+    digit.classList.add("digit");
+    digit.setAttribute("id", i);
+    digit.textContent = i;
+    digits.appendChild(digit);
+  }
+  const dot = document.createElement("button");
+  dot.classList.add("digit");
+  dot.textContent = ".";
+  digits.appendChild(dot);
+
+  const equal = document.createElement("button");
+  //equal.classList.add("operator");
+  equal.setAttribute("id", "equal");
+  equal.textContent = "=";
+  digits.appendChild(equal);
+}
+
+addDigits();
+
+let displayValue = 0;
+const display = document.querySelector("#display");
+
 function add(number, secondNumber) {
   return number + secondNumber;
 }
@@ -14,70 +41,64 @@ function divide(number, secondNumber) {
   return number / secondNumber;
 }
 
-let firstNumber;
-let operator;
-let secondNumber;
-let displayValue = 0;
-
 function operate(first, operator, second) {
   if (operator === "+") {
-    return (displayValue = add(first, second));
+    display.textContent = add(first, second);
   }
 
   if (operator === "-") {
-    console.log((displayValue = subtract(first, second)));
+    display.textContent = subtract(first, second);
   }
 
   if (operator === "*") {
-    displayValue = multiply(first, second);
+    display.textContent = multiply(first, second);
   }
 
-  if (operator === "/ ") {
-    displayValue = divide(first, second);
+  if (operator === "/") {
+    display.textContent = divide(first, second);
   }
 }
-
-operate(35, "-", 10);
-
-const digits = document.querySelector("#digits");
-const display = document.querySelector("#display");
-
-function addDigits() {
-  for (let i = 9; i > -1; i--) {
-    const digit = document.createElement("button");
-    digit.classList.add("digit");
-    digit.setAttribute("id", i);
-    digit.textContent = i;
-    digits.appendChild(digit);
-  }
-  const dot = document.createElement("button");
-  dot.classList.add("digit");
-  dot.textContent = ".";
-  digits.appendChild(dot);
-
-  const equal = document.createElement("button");
-  equal.classList.add("digit");
-  equal.textContent = "=";
-  digits.appendChild(equal);
-}
-
-addDigits();
 
 const allDigits = document.querySelectorAll(".digit");
-let num = "";
+const operators = document.querySelectorAll(".operator");
+const equal = document.querySelector("#equal");
 
-allDigits.forEach((digit) => {
-  digit.addEventListener("click", () => {
-    num += digit.id;
-    display.textContent = num;
-    console.log(num);
+let firstNumber;
+let operatorValue = "";
+let secondNumber;
+
+operators.forEach((operator) => {
+  operator.addEventListener("click", () => {
+    operatorValue = operator.id;
   });
 });
 
-const clearButton = document.querySelector("#clear-button")
+allDigits.forEach((digit) => {
+  digit.addEventListener("click", () => {
+    if (operatorValue == "") {
+      firstNumber = Number(digit.id);
+      display.textContent = firstNumber;
+      console.log("first", firstNumber);
+    }
 
-clearButton.addEventListener('click', () => {
-  num = ''
-  display.textContent = num;
-})
+    if (operatorValue !== "") {
+      secondNumber = Number(digit.id);
+      console.log("second", secondNumber);
+      display.textContent = secondNumber;
+    }
 
+    console.log("hey", firstNumber, secondNumber);
+  });
+});
+
+equal.addEventListener("click", () => {
+  operate(firstNumber, operatorValue, secondNumber);
+});
+
+const clearButton = document.querySelector("#clear-button");
+
+clearButton.addEventListener("click", () => {
+  firstNumber = 0;
+  secondNumber = 0;
+  display.textContent = "";
+});
